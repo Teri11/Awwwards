3b676520c9b7efa8f1f87908e4fab63bd78d97d9
Teri11 <augustineteri157@gmail.com>
2022-04-09 10:55:12 +0300

    bulk commit

--- awwwardsproject/urls.py
+++ awwwardsproject/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views 
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth import views,urls
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-#    path('',include('awwwardsapp.urls')),
+    path('admin/', admin.site.urls),
+    path('',include('awwwardsapp.urls'))           
     
 ]
